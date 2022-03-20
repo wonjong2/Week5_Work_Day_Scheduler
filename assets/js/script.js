f9c032a237811d2036d6d17c9e1cf467a6950c7a
Wonjong Park <wonjong2@gmail.com>
2022-03-20 15:32:47 -0700

    Uploaded the initial files with 1st implementation

---
+++ assets/js/script.js
@@ -0,0 +1,105 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// var ulEl = $("ul");
+var containerEl = $(".container");
+containerEl.addClass("timeblocks");
+
+var times = [["9AM", 9], ["10AM", 10], ["11AM", 11], ["12PM", 12], ["1PM", 13], ["2PM", 14], ["3PM", 15], ["4PM", 16], ["5PM", 17]];
+
+var timeInterval;
+
+containerEl.on("click", ".btn", saveEvent);
+
+function updateBgColor() {
+    var currentHour = moment().hour();
+    var currentMin;
+    var textareaEls = $("textarea");
+
+    clearTimeout(timeInterval);
+    
+    if(currentHour < 9 || currentHour > 17) {
+        return;
+    }
+
+    $(textareaEls[currentHour-9-1]).removeClass("present");
+    $(textareaEls[currentHour-9-1]).addClass("past");
+    $(textareaEls[currentHour-9]).removeClass("future");
+    $(textareaEls[currentHour-9]).addClass("present");
+
+    currentMin = moment().minute();
+    timeInterval = setTimeout(updateBgColor, 3600000-(currentMin*60000));
+}
+
+function saveEvent(event) {
+    // get the input element of place on where click event fires 
+    var siblingEls = $(event.currentTarget).siblings();
+    var time = siblingEls[0].textContent;
+    var eventItem = siblingEls[1].value;
+
+    if(eventItem === "") {
+        localStorage.removeItem(time);
+    }
+    else {
+        localStorage.setItem(time, eventItem);
+    }
+
+    return;
+}
+
+// create a list group with input groups
+function makeTimeblocks() {
+    var divEl;
+    var spanEl;
+    var inputEl;
+    var buttonEl;
+    var iconEl;
+    var storedEvent;
+    var currentHour;
+    var currentMin;
+
+     for(var i=0; i<9; i++){
+        // Create div element
+        divEl = $("<div>");
+        divEl.addClass("container row input-group");
+        // Create span element
+        spanEl = $("<span>");
+        spanEl.addClass("col-2 col-lg-1 text-end hour pt-2");
+        spanEl.text(times[i][0]);
+        divEl.append(spanEl);
+        // Create input element
+        inputEl = $("<textarea>");
+
+        currentHour = moment().hour();
+        if(times[i][1] < currentHour) {
+            inputEl.addClass("col-8 col-lg-10 form-control textarea past");
+        }
+        else if (times[i][1] === currentHour){
+            inputEl.addClass("col-8 col-lg-10 form-control textarea present");
+        }
+        else {
+            inputEl.addClass("col-8 col-lh-10 form-control textarea future");
+        }
+
+        storedEvent = localStorage.getItem(times[i][0]);
+        if(storedEvent){
+            inputEl.val(storedEvent);
+        }
+
+        divEl.append(inputEl);
+        // Create icon
+        iconEl = $("<i>");
+        iconEl.addClass("bi bi-save");
+        // Create buttone addons
+        buttonEl = $("<button>");
+        buttonEl.attr("type", "button");
+        buttonEl.addClass("col-2 col-lg-1 btn btn-outline-cecondary saveBtn");
+        buttonEl.append(iconEl);
+        divEl.append(buttonEl);
+        containerEl.append(divEl);
+    }
+    // Start the timer to update 
+    currentMin = moment().minute();
+    timeInterval = setTimeout(updateBgColor, 3600000-(currentMin*60000));
+}
+
+makeTimeblocks();
